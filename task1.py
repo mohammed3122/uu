790070318283fb83e8e8a8556731797523511ae7
mahometawad <mahometawad@gmail.com>
2024-11-23 21:13:07 +0000

    first commit

---
+++ task1.py
@@ -0,0 +1,12 @@
+# رسالة ترحيب
+print('Hi My Prof in My Task !')
+
+#بيانتي الشخصية
+print(
+    
+    '''
+    My name is Mohammad Awd
+    My Age is 22 Years
+    My favorite Animal is Cat
+    '''
+)
---
+++ task2.py
@@ -0,0 +1,18 @@
+intNumber = 5
+print('Interger Number is : ' , intNumber)
+
+floatNumber = 3.5
+print('Foat Number is :', floatNumber)
+
+complexNumber = 6j
+print('Complex Number is :', complexNumber)
+
+stringData = 'Mohammad Awd'
+print('String Data is :', stringData)
+
+ 
+booleanSex= 'male'
+if booleanSex == 'male':
+    print('Result of Sex : True')
+else:
+    print('Result of Sex : False')
